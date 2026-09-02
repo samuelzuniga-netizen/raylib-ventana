062a1aaf35240e6672ca98d98103dee21a0ccc14
Samuel Zuniga <bluecanvas@Samuels-MacBook-Air.local>
2026-09-02 12:10:38 -0700

    Initial commit: Proyecto raylib con ventana

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Ventana Gráfica");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.RayWhite);
+
+    Raylib.DrawText("¡Hola, mundo!", 190, 200, 20, Color.Maroon);
+
+    Raylib.EndDrawing();
+}
+Raylib.CloseWindow();
