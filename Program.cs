41fae642e447b83a012270c46e1a35db3a97dfd0
samuelzuniga-netizen <samuel.zuniga@cetys.edu.mx>
2026-09-04 12:29:07 -0700

    program nuevo dibujo

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ internal static class Program
     public static void Main(){
     const int anchoVentana = 800;
     const int altoVentana = 600;
-    const int celda = 100;
+    const int celda = 30;
     const int origenX = 100;
-    const int origenY = 100;
+    const int origenY = 50;
 
     Raylib.InitWindow(anchoVentana, altoVentana, "Pixcel lab 16x16");
     Raylib.SetTargetFPS(60);
@@ -20,19 +20,36 @@ internal static class Program
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.RayWhite);
         int[,] imagen = {
-        {1, 0, 0, 1},
-        {0, 0, 0, 0},
-        {1, 0, 0, 1},
-        {0, 1, 1, 0}
+        {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+        {0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0},
+        {0, 0, 1, 2, 2, 1, 1, 1, 1, 1, 0, 1, 2, 1, 0, 0},
+        {0, 0, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 0, 0},
+        {0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1, 0, 0},
+        {0, 0, 0, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1, 0, 0, 0},
+        {0, 0, 0, 1, 3, 2, 2, 2, 2, 2, 2, 3, 1, 0, 0, 0},
+        {0, 0, 0, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 0, 0, 0},
+        {0, 0, 0, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1, 0, 0, 0},
+        {0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 0, 0},
+        {0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0},
+       
     };
 
-    for (int fila = 0; fila < 4; fila++)
-    for (int col = 0; col < 4; col++)
+    for (int fila = 0; fila < 16; fila++)
+    for (int col = 0; col < 16; col++)
     {
         int x = origenX + col * celda;
         int y = origenY + fila * celda;
         if (imagen[fila, col] == 1)
-        Raylib.DrawRectangle(x, y, celda, celda, Color.SkyBlue);
+        Raylib.DrawRectangle(x, y, celda, celda, Color.Black);
+        else if (imagen[fila, col] == 2)
+        Raylib.DrawRectangle(x, y, celda, celda, Color.Yellow);
+        else if(imagen[fila, col] == 3)
+        Raylib.DrawRectangle(x, y, celda, celda, Color.Red);
     }
         Raylib.EndDrawing();
     }
--- obj/Debug/net10.0/Trabajo_2.AssemblyInfo.cs
+++ obj/Debug/net10.0/Trabajo_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabajo_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a743984cf1c380e6c58712ca56a8891e68927c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768fdb48a20d89086a0e201334071f72d49a8ad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabajo_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabajo_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
