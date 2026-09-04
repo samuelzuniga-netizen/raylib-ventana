768fdb48a20d89086a0e201334071f72d49a8ad2
samuelzuniga-netizen <samuel.zuniga@cetys.edu.mx>
2026-09-04 11:45:40 -0700

    Arreglo: Declarar constantes celda, origenX y origenY para grid de píxeles

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Raylib_cs;
+﻿using System.Runtime.Serialization;
+using Raylib_cs;
 
 namespace S079PixelLab;
 
@@ -7,6 +8,9 @@ internal static class Program
     public static void Main(){
     const int anchoVentana = 800;
     const int altoVentana = 600;
+    const int celda = 100;
+    const int origenX = 100;
+    const int origenY = 100;
 
     Raylib.InitWindow(anchoVentana, altoVentana, "Pixcel lab 16x16");
     Raylib.SetTargetFPS(60);
@@ -15,10 +19,21 @@ internal static class Program
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.RayWhite);
+        int[,] imagen = {
+        {1, 0, 0, 1},
+        {0, 0, 0, 0},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0}
+    };
 
-        Raylib.DrawText("Pixel lab 16x16", 20, 20, 20, Color.Maroon);
-        Raylib.DrawText("Esc: cerrar", 20, 52, 18, Color.Gray);
-
+    for (int fila = 0; fila < 4; fila++)
+    for (int col = 0; col < 4; col++)
+    {
+        int x = origenX + col * celda;
+        int y = origenY + fila * celda;
+        if (imagen[fila, col] == 1)
+        Raylib.DrawRectangle(x, y, celda, celda, Color.SkyBlue);
+    }
         Raylib.EndDrawing();
     }
     Raylib.CloseWindow();
